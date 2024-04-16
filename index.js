a46f8af0db4484a6671254a086c4f668eef7d430
senanurkorkutan <snnrkrk1@gmail.com>
2024-04-16 15:29:17 +0200

    :fire: so much things happend

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // IMPORTING
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 // PORT
 const PORT = process.env.PORT || 3333;
 
+// .ENV FILE
+const dotenv = require("dotenv");
+dotenv.config();
+
 // BASIC SETTINGS
+
+// api_key
+const apiKey = process.env.TMDB_API_KEY;
+// console.log("API Key:", apiKey); // Check if API key is loaded correctly
+console.log("Loaded API Key:", process.env.TMDB_API_KEY);
+
+// const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+// console.log("API URL:", apiUrl);
 
 // templating engine
 app.set("view engine", "ejs");
@@ -14,25 +27,65 @@ app.set("views", "./views");
 // om public zichtbaar te maken
 app.use(express.static("public"));
 
-// .ENV FILE
-const dotenv = require("dotenv");
-dotenv.config();
-
 // API
 //
 //
 
 // ROUTING
-app.get("/", (req, res) => {
-    console.log(req.query);
-    res.render("index", { title: "Homety" });
-    // res.send("HoOo")
+app.get("/", async (req, res) => {
+    const movieData = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`).then((res) => res.json());
+    const trendingData = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`).then((res) => res.json());
+    console.log(movieData, trendingData);
+    res.render("pages/index", { title: "TEST", movies: movieData.results, trendingMovies: trendingData.results });
 });
 
+// DETAIL MOVIE
+app.get("/movie/:id/", async (req, res) => {
+    const movieId = req.params.id;
 
-app.get("/home", (req, res) => {
-    console.log(req.query);
-    res.render("index", { title: "Homety" });
+    const movie = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`).then((res) => res.json());
+    res.render("pages/detail", { title: "Detail", movie: movie });
+});
+
+// got help from chatGPT
+// app.get("/movies", async (req, res) => {
+
+// });
+
+// JSON FOR DISCOVER
+app.get("/movies-json", async (req, res) => {
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+    console.log("API URL:", apiUrl);
+
+    try {
+        const response = await axios.get(apiUrl);
+        console.log("API request successful");
+        const movies = response.data.results;
+        res.json(movies);
+        // res.render("pages/index", { title: "TEST", movies: movies });
+    } catch (error) {
+        console.error("Error fetching movies:", error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get("/movie", async (req, res) => {
+    const url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+    const options = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+        },
+    };
+
+    fetch(url, options)
+        .then((res) => res.json())
+        .then((trendingJson) => {
+            console.log(trendingJson);
+            res.render("pages/index.ejs");
+        })
+        .catch((err) => console.error("error:" + err));
 });
 
 app.get("/about", (req, res) => {
@@ -49,12 +102,12 @@ app.listen(PORT, () => {
 
 // LAATSTE ROUTE ERROR(404) PAGINA
 // app.get("/*", (req, res) => {
-//     res.status(404).render("404", { title: "404" });
+//     res.status(404).render("pages/404", { title: "404" });
 // });
 
 app.use(function (req, res) {
     res.locals.title = "Error 404";
-    res.status(404).render("404", {
+    res.status(404).render("pages/404", {
         path: "Error",
     });
 });
--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
 console.log("are you working?");
 
+// Search button
 const searchBox = document.getElementById("searchBox"),
     searchIcon = document.getElementById("searchIcon");
 
 searchIcon.onclick = function () {
     searchBox.classList.toggle("active");
 };
+
+// TMDB fetching
+
+// const API_KEY = "api_key=04625eb1806d881fb98867452db70e10";
+
+// const TMDB_URL = "https://api.themoviedb.org/3/";
